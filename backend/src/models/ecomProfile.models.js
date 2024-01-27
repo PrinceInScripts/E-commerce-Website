aed8697714864282bfbed6b1fcf910ddf3d17427
princekumar <princepk7878kumar@gmail.com>
2024-01-27 19:37:08 +0530

    done ecom profile model

---
+++ backend/src/models/ecomProfile.models.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+
+const ecomProfileSchema=new Schema({
+   firstName:{
+    type:String,
+    default:"John"
+   },
+   lastName:{
+    type:String,
+    default:"Doe"
+   },
+   countryCode:{
+    type:String,
+    default:""
+   },
+   phoneNumber:{
+    type:String,
+    default:""
+   },
+   owner:{
+    type:Schema.Types.ObjectId,
+    ref:"User",
+   },
+},{timestamps:true})
+
+export const EcomProfile=mongoose.model('EcomProfile',ecomProfileSchema)
