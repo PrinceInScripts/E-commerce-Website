bd9a4fb9b397e97c2b69828f5cb7bd04b94f97f9
princekumar <princepk7878kumar@gmail.com>
2024-01-27 18:49:17 +0530

    done constant.js

--- backend/src/constant.js
+++ backend/src/constant.js
@@ -1,2 +1,40 @@
 
 export const DB_NAME="e-commerce"
+
+export const userRolesEnum={
+    ADMIN:"ADMIN",
+    USER:"USER"
+}
+
+export const AvailableUserRoles=Object.values(userRolesEnum)
+
+export const orderStatusEnum={
+    PENDING:"PENDING",
+    CANCELLED:"CANCELLED",
+    DELIVERED:"DELIVERED",
+}
+
+export const AvailableOrderStatus=Object.values(orderStatusEnum)
+
+export const PaymentProviderEnum={
+    UNKNOWN:"UNKNOWN",
+    RAZORPAY:"RAZORPAY",
+    PAYPAL:"PAYPAL",
+}
+
+export const AvailablePaymentProvider=Object.values(PaymentProviderEnum)
+
+export const CoupenTypeEnum = {
+    FLAT:"FLAT",
+}
+
+export const AvailableCouponType=Object.values(CoupenTypeEnum)
+
+export const userLoginType={
+    GOOGLE:"GOOGLE",
+    GITHUB:"GITHUB",
+    EMAIL_PASSWORD:"EMAIL_PASSWORD",
+}
+
+export const AvailableUserLoginType=Object.values(userLoginType)
+
