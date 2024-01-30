c4f3fb454986e746f6b6e0920550b9d957b5dfc3
princekumar <princepk7878kumar@gmail.com>
2024-01-30 10:41:28 +0530

    done getAllAddress controller

--- backend/src/controllers/address.controller.js
+++ backend/src/controllers/address.controller.js
@@ -2,6 +2,7 @@ import { Address } from "../models/address.models.js";
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import {} from "../utils/ApiError.js"
+import { getMongoosePaginationOptions } from "../utils/helpers.js"
 
 const createAddress=asyncHandler(async(req,res)=>{
     const {addressLine1,addressLine2,city,state,country,pincode}=req.body
@@ -38,8 +39,42 @@ const createAddress=asyncHandler(async(req,res)=>{
     )
 })
 
+const getAllAddress=asyncHandler(async(req,res)=>{
+    const {page=1,limit=10}=req.query;
+    const addressAggregation=Address.aggregate([
+        {
+            $match:{
+                owner:req.user._id,
+            }
+        }
+    ])
+
+    const addresses = await Address.aggregatePaginate(
+        addressAggregation,
+        getMongoosePaginationOptions({
+            page,
+            limit,
+            customLabels:{
+                totalDocs:"totalAddresses",
+                docs:"addresses"
+            }
+        })
+    )
+
+    return res
+              .status(200)
+              .json(
+                new ApiResponse(
+                    200,
+                    addresses,
+                    "Address fetched succesfully"
+                )
+              )
+})
+
 
 
 export {
-    createAddress
+    createAddress,
+    getAllAddress
 }
--- backend/src/routes/address.routes.js
+++ backend/src/routes/address.routes.js
@@ -2,7 +2,7 @@ import {Router} from "express"
 import { createAddressValidator } from "../validators/app/address.validators.js"
 import { verifyJWT } from "../middlewares/auth.middlewares.js"
 import { validate } from "../validators/validate.js"
-import { createAddress } from "../controllers/address.controller.js"
+import { createAddress, getAllAddress } from "../controllers/address.controller.js"
 
 const router=Router()
 
@@ -10,5 +10,6 @@ router.use(verifyJWT)
 
 router.route("/")
               .post(createAddressValidator(),validate,createAddress)
+              .get(getAllAddress)
 
 export default router
