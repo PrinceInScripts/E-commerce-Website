cdc650aba09ebe130e883c90bbdba1d01323040e
princekumar <princepk7878kumar@gmail.com>
2024-01-30 08:45:24 +0530

    done adress model

---
+++ backend/src/models/address.models.js
@@ -0,0 +1,41 @@
+import moongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+
+const addressSchema = new Schema(
+  {
+    adressLine1: {
+      type: String,
+      required: true,
+    },
+    addressLine2:{
+        type:String,
+    },
+    city:{
+        type:String,
+        required:true,
+    },
+    country:{
+        type:String,
+        required:true,
+    },
+    owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User",
+    },
+    pincode:{
+        type:String,
+        required:true,
+    },
+    state:{
+        type:String,
+        required:true,
+    }
+  },
+  { timestamps: true }
+);
+
+
+addressSchema.plugin(mongooseAggregatePaginate);
+
+export const Address = moongoose.model("Address", addressSchema);
