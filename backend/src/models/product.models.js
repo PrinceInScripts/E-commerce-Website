c804b334d5872e873219c1ae9e21b18b4377bec6
princekumar <princepk7878kumar@gmail.com>
2024-01-30 15:35:43 +0530

    done product model

---
+++ backend/src/models/product.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const productSchema = new Schema({
+        category:{
+            type:Schema.Types.ObjectId,
+            ref:"Category",
+            required:true
+        },
+        description:{
+            type:String,
+            required:true,
+        },
+        mainImage:{
+            type:String,
+            required:true,
+        },
+        name:{
+            type:String,
+            required:true,
+        },
+        owner:{
+            type:Schema.Types.ObjectId,
+            ref:"User",
+        },
+        price:{
+            type:Number,
+            default:0
+        },
+        stock:{
+            type:Number,
+            default:0
+        },
+        subImages:{
+            type:[
+                {
+                    type:String,
+                }
+            ],
+            default:[]
+        }
+
+},{timestamps:true})
+
+productSchema.plugin(mongooseAggregatePaginate);
+
+export const Product=mongoose.model("Product",productSchema)
