fa333219ca2a43c83bd73106464f9bcb58fd1712
princekumar <princepk7878kumar@gmail.com>
2024-01-31 19:45:23 +0530

    done getCart methdod controller

---
+++ backend/src/controllers/cart.controller.js
@@ -0,0 +1,80 @@
+
+import {} from "../utils/ApiError.js"
+import {} from "../utils/ApiResponse.js"
+import {} from "../utils/asyncHandler.js"
+
+export const getCart=async (userId)=>{
+    const cartAggregation=await Cart.aggregate([
+        {
+            $match:{
+                owner:userId
+            },
+        },
+        {
+            $unwind:"$items"
+        },
+        {
+            $lookup:{
+                from:"products",
+                localField:"items.productId",
+                foreignField:"_id",
+                as:"product"
+            },
+        },
+        {
+            $project:{
+                product:{$first:"$product"},
+                quantity:"$items.quantity",
+                coupon:1,
+            },
+        },
+        {
+            $group:{
+                _id:"$_id",
+                items:{
+                    $push:"$$ROOT",
+                },
+                coupon:{$first:"$coupon"},
+                cartTotal:{
+                    $sum:{
+                        $multiply:["$product.price","$quantity"]
+                    }
+                }
+            }
+        },
+        {
+            $lookup:{
+                from:"coupons",
+                localField:"coupon",
+                foreignField:"_id",
+                as:"coupon"
+            }
+        },
+        {
+            $addfields:{
+                coupon:{$first:"$coupon"},
+            }
+        },
+        {
+            $addfields:{
+                discountedTotal:{
+                    $ifNull:[
+                        {
+                            $subtract:["$cartTotal","$coupon.discountValue"],
+                        },
+                        "$cartTotal",
+                    ]
+                }
+            }
+        }
+    ]);
+
+    return (
+        cartAggregation[0] ?? {
+            _id:null,
+            items:[],
+            cartTotal:0,
+            discountedTotal:0,
+        }
+    )
+}
